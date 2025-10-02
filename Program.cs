03047bbd9418b28aec0327b9a69917b679cb4871
Orionpax <emil2008abdullaev@gmail.com>
2025-10-02 18:41:21 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 //*******************************************************************************************
 // *Практическая работа №9                                                                  *
 //* Выполнил: Абдуллаев Э.С., группа 2-ИСПд                                                 *
-//* Задание:  массив = 10 найти кол-во и сумму элементов, значения которых кратны Мю        *
+//* Задание:  массив = 10 найти кол-во и сумму элементов, значения которых кратны М         *
 //*******************************************************************************************
 using System;
 using System.Collections.Generic;
@@ -17,68 +17,103 @@ namespace Pr_9
     {
         static void Main(string[] args)
         {
-            try
+            bool c = true;
+            while (c)
             {
-                Console.Title = "Практическая работа №9";
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Здравствуйте!");
-                Console.ForegroundColor = ConsoleColor.Blue;
-                int count = 0;
-                int sum = 0;
-                int[] A = new int[10];
-                Console.WriteLine("Введите 10 целых чисел для массива:");
-                for (int i = 0; i < A.Length; i++)
+                try
                 {
-                    Console.Write($"Элемент {i + 1}: ");
-                    A[i] = Convert.ToInt32(Console.ReadLine());
-                }
-                int M;
-                while (true)
-                {
-                    Console.Write("Введите значение M: ");
-                    M = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                    Console.Title = "Практическая работа №9";
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Здравствуйте!");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    int count = 0;
+                    int sum = 0;
+                    int[] A = new int[10];
+                    Console.WriteLine("Введите 10 целых чисел для массива:");
+                    for (int i = 0; i < A.Length; i++)
+                    {
+                        Console.Write($"Элемент {i + 1}: ");
+                        A[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    int M;
+                    while (true)
+                    {
+                        Console.Write("Введите значение M: ");
+                        M = Convert.ToInt32(Console.ReadLine());
 
-                    if (M == 0)
-                    {
-                        Console.WriteLine("Не верное значение");
+                        if (M == 0)
+                        {
+                            Console.WriteLine("Не верное значение");
+                        }
+                        else
+                        {
+                            break;
+                        }
                     }
-                    else
+                    foreach (int num in A)
                     {
-                        break;
+                        if (num != 0 && num % M == 0)
+                        {
+                            count++;
+                            sum += num;
+                        }
+                    }
+                    Console.WriteLine($"Количество элементов кратных M: {count}");
+                    Console.WriteLine($"Сумма элементов: {sum}");
+                    Console.WriteLine("Хотите выполнить новый расчет?");
+                    Console.WriteLine("1 - новый расчет");
+                    Console.WriteLine("0 - выход из программы");
+                    string w = Console.ReadLine();
+                    switch (w)
+                    {
+                        case "1":
+                            Console.WriteLine("Новый расчет:");
+                            Console.WriteLine("Нажмите Enter для продолжения...");
+                            Console.ReadKey();
+                            continue;
+                        case "0":
+                            c = false;
+                            break;
+                        default:
+                            Console.WriteLine("Неизвестная команда");
+                            Console.Clear();
+                            Console.ReadKey();
+                            break;
                     }
                 }
-                foreach (int num in A)
+                catch (IndexOutOfRangeException iEx)
                 {
-                    if (num != 0 && num % M == 0)
-                    {
-                        count++;
-                        sum += num;
-                    }
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Произошла ошибка: {iEx.Message}");
+                    Console.ResetColor();
+                    continue;
                 }
-                Console.WriteLine($"Количество элементов кратных M: {count}");
-                Console.WriteLine($"Сумма элементов: {sum}");
+                catch (FormatException fEx)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Ошибка:{fEx.Message}");
+                    Console.ResetColor();
+                    continue;
+                }
+                catch (OverflowException oEx)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Ошибка: {oEx.Message}");
+                    Console.ResetColor();
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                    Console.ResetColor();
+                    continue;
+                }
+                Console.Clear();
+                Console.WriteLine("Программа завершена, нажмите любую клавишу для выхода");
+                Console.ReadKey();
             }
-            catch (IndexOutOfRangeException iEx)
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Произошла ошибка: {iEx.Message}");
-            }
-            catch (FormatException fEx)
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Ошибка:{fEx.Message}");
-            }
-            catch (OverflowException oEx)
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Ошибка: {oEx.Message}");
-            }
-            catch (Exception ex)
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Произошла ошибка: {ex.Message}");
-            }
-            Console.ReadKey();
         }
     }
 }
